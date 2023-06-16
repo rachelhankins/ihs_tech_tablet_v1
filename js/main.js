2489cd0dbbe4a063311c8242e6f93995720edc8c
rachelhankins <rachel.hankins@dish.com>
2023-06-16 16:22:20 -0600

    Add baseline code

---
+++ js/main.js
@@ -0,0 +1,8 @@
+if (document.location.href.indexOf('domain') > -1 ||
+document.location.href.indexOf('test') > -1) {
+
+$(document).ready(function() {
+    $('video').attr('autoplay','autoplay')
+
+});
+} 
